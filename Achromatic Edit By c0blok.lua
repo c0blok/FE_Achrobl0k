cae80d2130beb4acd65c02dfbb05078fac163806
c0blok <73387478+c0blok@users.noreply.github.com>
2021-05-29 03:19:39 -0500

    Update Achromatic Edit By c0blok.lua

--- Achromatic Edit By c0blok.lua
+++ Achromatic Edit By c0blok.lua
@@ -1,4 +1,4 @@
 	local notLoaded = Instance.new("Message",workspace)
-	notLoaded.Text = '[c0blok]: FE Achoobl0k Soon!'
+	notLoaded.Text = '[c0blok]: FE Achroobl0k Soon!'
 wait(3)
   notLoaded:Destroy()
